8839a2e1dafaa45be07dd1d614143a46a7da435a
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-30 19:05:09 +0500

    weather dislayed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,76 @@
-import { ReactComponent } from '*.svg';
+
 import React from 'react'
 
+const PLACES = [
+  { name: "Palo Alto", zip: "94303" },
+  { name: "San Jose", zip: "94088" },
+  { name: "Santa Cruz", zip: "95062" },
+  { name: "Honolulu", zip: "96803" }
+];
+
 class WeatherDisplay extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      weatherData: null
+    }
+  }
+  componentDidMount() {
+    const zip = this.props.zip
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+    zip +
+    "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then(response => response.json())
+              .then(json => {
+                this.setState({weatherData: json})
+              })
+  }
   render() {
+    const weatherData = this.state.weatherData
+    const weather = weatherData.weather[0]
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+
+    if (!weatherData) return <div>Loading....</div>
+
     return (
-      <h1>Display some weather</h1>
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed}</p>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      activePlace: 0
+    }
+  }
+  render() {
+    const activePlace = this.state.activePlace
+    return (
+      <div className="App">
+        
+        {PLACES.map((place, index) => (
+          <button key={index} 
+                  onClick={() => this.setState({activePlace: index})}>
+                  {place.name}
+                  </button>
+        ))}
+
+        <WeatherDisplay 
+                zip={PLACES[activePlace].zip} 
+                key={activePlace} 
+                ></WeatherDisplay>
+      </div>
     )
   }
 }
