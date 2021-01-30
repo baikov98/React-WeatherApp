b101efffaa99c9c9bef7d87b4fea4908e76de4ef
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-30 19:25:31 +0500

    app is working

--- src/App.js
+++ src/App.js
@@ -23,14 +23,16 @@ class WeatherDisplay extends React.Component {
     fetch(URL).then(response => response.json())
               .then(json => {
                 this.setState({weatherData: json})
+                console.log(this.state)
               })
   }
   render() {
+    
     const weatherData = this.state.weatherData
+    if (!weatherData) return <div>Loading....</div>
     const weather = weatherData.weather[0]
     const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
 
-    if (!weatherData) return <div>Loading....</div>
 
     return (
       <div>
